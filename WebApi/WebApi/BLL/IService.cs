188e6ac18cafc711969543999f635b781ba69141
Michal Cichosz <92848032+michcich@users.noreply.github.com>
2024-06-26 10:23:36 +0200

    Add files via upload

---
+++ WebApi/WebApi/BLL/IService.cs
@@ -0,0 +1,11 @@
+﻿using WebApi.DTO;
+
+namespace WebApi.Service
+{
+    public interface IService
+    {
+        IEnumerable<MouseResponseDTO> GetAll();
+        void Delete(int mouseId);
+        void Create(MouseRequestDTO requestDto);
+    }
+}
